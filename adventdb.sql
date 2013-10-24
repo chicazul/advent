d449d74df70a2146787504474b912ee2ab7b0231
Sara Chicazul <sara@chicazul.com>
2013-10-23 21:24:34 -0700

    Agh foreign keys are bullshit what

--- adventdb.sql
+++ adventdb.sql
@@ -1,24 +1,18 @@
 -- Advent schema, optimised for MySQL
 
--- generic text posts
+DROP TABLE IF EXISTS imagetags;
+DROP TABLE IF EXISTS posttags;
+DROP TABLE IF EXISTS producttags;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS productimages;
+DROP TABLE IF EXISTS productoptions;
+DROP TABLE IF EXISTS producttypes;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS posts;
-CREATE TABLE posts (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	title varchar(128) NOT NULL,
-	slug varchar(128) NOT	NULL,
-	author int(11) NOT NULL,
-	created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	blurb varchar(256) NOT NULL,
-	content text NOT NULL,
-	PRIMARY KEY (id),
-	KEY slug (slug),
-	FOREIGN KEY (author)
-		REFERENCES users(id)
-);
+DROP TABLE IF EXISTS users;
 
 -- maybe one day will have need of multiple user accounts
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	username varchar(100) NOT NULL,
@@ -28,8 +22,24 @@ CREATE TABLE users (
 	UNIQUE KEY username (username)
 );
 
+-- generic text posts
+CREATE TABLE posts (
+	id int(11) NOT NULL AUTO_INCREMENT,
+	title varchar(128) NOT NULL,
+	slug varchar(128) NOT NULL,
+	author int(11) NOT NULL,
+	created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	blurb varchar(256) NOT NULL,
+	content text NOT NULL,
+	PRIMARY KEY (id),
+	KEY slug (slug),
+	FOREIGN KEY (author)
+		REFERENCES users(id)
+		ON DELETE RESTRICT
+);
+
 -- photos etc (still havent thought out schema for this)
-DROP TABLE IF EXISTS images;
 CREATE TABLE images (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	artist varchar(128) NOT NULL,
@@ -41,7 +51,6 @@ CREATE TABLE images (
 );
 
 -- products to sell
-DROP TABLE IF EXISTS products;
 CREATE TABLE products (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	productname varchar(100) NOT NULL,
@@ -52,22 +61,11 @@ CREATE TABLE products (
 	PRIMARY KEY (id),
 	FOREIGN KEY (thumbnailid)
 		REFERENCES images(id)
-);
-
--- optional features for products. May revise
-DROP TABLE IF EXISTS productoptions;
-CREATE TABLE productoptions (
-	id int(11) NOT NULL AUTO_INCREMENT,
-	optionname varchar(50) NOT NULL,
-	type varchar(50) NOT NULL,
-	surcharge decimal(19,4),
-	PRIMARY KEY (id),
-	FOREIGN KEY (type)
-		REFERENCES producttypes(type)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
 );
 
 -- types for product options
-DROP TABLE IF EXISTS producttypes;
 CREATE TABLE producttypes (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	productid int(11) NOT NULL,
@@ -76,23 +74,35 @@ CREATE TABLE producttypes (
 	INDEX (type),
 	FOREIGN KEY (productid)
 		REFERENCES products(id)
+		ON DELETE CASCADE
 );
 
+-- optional features for products. May revise
+CREATE TABLE productoptions (
+	id int(11) NOT NULL AUTO_INCREMENT,
+	optionname varchar(50) NOT NULL,
+	type varchar(50) NOT NULL,
+	surcharge decimal(19,4),
+	PRIMARY KEY (id),
+	FOREIGN KEY (type)
+		REFERENCES producttypes(type)
+		ON DELETE CASCADE
+);
 
 -- tags for products
-DROP TABLE IF EXISTS productimages;
 CREATE TABLE productimages (
 	productid int(11) NOT NULL,
 	imageid int(11) NOT NULL,
 	PRIMARY KEY (productid,imageid),
 	FOREIGN KEY (productid)
-		REFERENCES products(id),
+		REFERENCES products(id)
+		ON DELETE CASCADE,
 	FOREIGN KEY (imageid)
 		REFERENCES images(id)
+		ON DELETE CASCADE
 );
 
 -- normalised tag table
-DROP TABLE IF EXISTS tags;
 CREATE TABLE tags (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	tagname varchar(50),
@@ -101,37 +111,40 @@ CREATE TABLE tags (
 );
 
 -- tags for products
-DROP TABLE IF EXISTS producttags;
 CREATE TABLE producttags (
 	productid int(11) NOT NULL,
 	tagid int(11) NOT NULL,
 	PRIMARY KEY (productid,tagid),
 	FOREIGN KEY (productid)
-		REFERENCES products(id),
+		REFERENCES products(id)
+		ON DELETE CASCADE,
 	FOREIGN KEY (tagid)
 		REFERENCES tags(id)
+		ON DELETE CASCADE
 );
 
 -- tags for text posts
-DROP TABLE IF EXISTS posttags;
 CREATE TABLE posttags (
 	postid int(11) NOT NULL,
 	tagid int(11) NOT NULL,
 	PRIMARY KEY (postid,tagid),
 	FOREIGN KEY (postid)
-		REFERENCES posts(id),
+		REFERENCES posts(id)
+		ON DELETE CASCADE,
 	FOREIGN KEY (tagid)
 		REFERENCES tags(id)
+		ON DELETE CASCADE
 );
 
 -- tags for images
-DROP TABLE IF EXISTS imagetags;
 CREATE TABLE imagetags (
 	imageid int(11) NOT NULL,
 	tagid int(11) NOT NULL,
 	PRIMARY KEY (imageid,tagid),
 	FOREIGN KEY (imageid)
-		REFERENCES images(id),
+		REFERENCES images(id)
+		ON DELETE CASCADE,
 	FOREIGN KEY (tagid)
 		REFERENCES tags(id)
+		ON DELETE CASCADE
 );
