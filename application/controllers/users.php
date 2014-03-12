8ca5fd3e7668a68ecf9f29cae4616bb22a8d9604
Sara Chicazul <sara@chicazul.com>
2014-03-12 14:49:04 -0700

    Fold login code into user model

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -15,6 +15,44 @@ class Users extends CI_Controller {
 			$this->login_manager->logout();
 		}
 	}
+
+	public function login()
+	{
+		$this->load->library('login_manager', array('autologin' => FALSE));
+		$this->load->helper('form');
+		$this->load->helper('array');
+		$user = $this->login_manager->get_user();
+		if($user !== FALSE)
+		{
+			// already logged in, redirect <-- wait what does this mean there is no redirect here?
+		}
+
+		$user = new User();
+		if($this->input->post('username') !== FALSE)
+		{
+			// load data from attempted login
+			$user->from_array($_POST, array('username','password'));
+			// call validation of login request
+			$login_redirect = $this->login_manager->process_login($user);
+			if($login_redirect)
+			{
+				if($login_redirect === TRUE)
+				{
+					// if no redirection page, redirect to welcome page
+					redirect('welcome');
+				}
+				else
+				{
+					// otherwise, redirect to stored last-accessed page
+					redirect($login_redirect);
+				}
+			}
+		}
+
+		$this->load->view('templates/header',array('title' => 'Log In'));
+		$this->load->view('users/login',array('user' => $user));
+		$this->load->view('templates/footer');
+	}
 	public function index()
 	{
 		$this->load->library('form_validation');
